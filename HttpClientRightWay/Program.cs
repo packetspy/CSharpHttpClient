00298eab453af3577295141eda9db68a677a43cd
Anderson Tomazeto <packetspy@gmail.com>
2023-09-12 10:37:41 -0300

    Testing HttpClient

---
+++ HttpClientRightWay/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+public class Program
+{
+    private static HttpClient client = new HttpClient();
+    public static async Task Main(string[] args)
+    {
+        var url = "https://www.google.com";
+        int nloop = 10;
+
+        Console.WriteLine($"Starting connections with {url}");
+
+        for (int i = 0; i < nloop; i++)
+        {
+            var result = await client.GetAsync(url);
+            Console.WriteLine($"{url} -> {result.StatusCode}");
+        }
+
+        Console.WriteLine("End connections");
+    }
+}
---
+++ HttpClientWrongWay/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var url = "https://www.google.com";
+int nloop = 10;
+
+Console.WriteLine($"Starting connections with {url}");
+
+for (int i = 0; i<nloop; i++)
+{
+	using (var client = new HttpClient())
+	{
+		var result = await client.GetAsync(url);
+		Console.WriteLine($"{url} -> {result.StatusCode}");
+	}
+}
+
+Console.WriteLine("End connections");
